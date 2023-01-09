b822cc0ab79da17162c2b3ef13a6abb9c45d3c41
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-01-09 15:34:26 +0900

    refactored

---
+++ NodoAme.Core/Models/TalkSoft/TalkEngine.cs
@@ -0,0 +1,10 @@
+namespace NodoAme;
+
+/// <summary>
+/// トークエンジン列挙
+/// </summary>
+public static class TalkEngine{
+	public const string CEVIO = "CeVIO";
+	public const string OPENJTALK = "OpenJTalk";
+	public const string VOICEVOX = "VOICEVOX";
+}
