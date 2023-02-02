fccae024d43dacf5d1138b98e02507a74fdb6d11
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-02 12:59:46 +0900

    format

--- NodoAme.Core/Models/PhonemeUtil.cs
+++ NodoAme.Core/Models/PhonemeUtil.cs
@@ -3,8 +3,8 @@ using System.Text.RegularExpressions;
 
 namespace NodoAme.Models
 {
-    public static class PhonemeUtil
-    {
+	public static class PhonemeUtil
+	{
 		public const string CL = "cl";
 		public const string PAU = "pau";
 		public const string SIL = "sil";
@@ -12,7 +12,6 @@ namespace NodoAme.Models
 
 		public static Regex VOWELS_JA = new("[aiueoAIUEO]", RegexOptions.Compiled);
 		public static Regex NOSOUND_VOWELS = new("[AIUEO]", RegexOptions.Compiled);
-
 		public static Regex NO_CONSONANT = new($"{INVALID_PH}|{CL}|{PAU}|{SIL}", RegexOptions.Compiled);
 		public static Regex NASAL_JA = new("[nmN]|ng", RegexOptions.Compiled);
 
@@ -22,17 +21,17 @@ namespace NodoAme.Models
 		/// <param name="pText"></param>
 		/// <returns></returns>
 		public static bool IsVowel(string? pText){
-            if(string.IsNullOrEmpty(pText)) return false;
+			if(string.IsNullOrEmpty(pText)) return false;
 			return VOWELS_JA.IsMatch(pText);
 		}
 
-        /// <summary>
+		/// <summary>
 		/// ラベルの音素が母音かどうか
 		/// </summary>
 		/// <param name="label"></param>
 		/// <returns></returns>
 		public static bool IsVowel(Label label){
-            if(label is null)return false;
+			if(label is null)return false;
 			return PhonemeUtil.IsVowel(label.Phoneme);
 		}
 
@@ -44,74 +43,73 @@ namespace NodoAme.Models
 		public static bool IsNoSoundVowel(string text)
 			=> NOSOUND_VOWELS.IsMatch(text);
 
-
 		/// <summary>
 		/// 音素が子音かどうか
 		/// </summary>
 		/// <param name="cText"></param>
 		/// <returns></returns>
 		public static bool IsConsonant(string? cText){
-            if(string.IsNullOrEmpty(cText)) return false;
+			if(string.IsNullOrEmpty(cText)) return false;
 
-            if(NO_CONSONANT.IsMatch(cText)){
-                //no:子音
+			if(NO_CONSONANT.IsMatch(cText)){
+				//no:子音
 				return false;
 			}else if(IsVowel(cText)){
 				//no:母音
 				return false;
 			}else{
 				//yes
-                return true;
-            }
-        }
+				return true;
+			}
+		}
 
-        public static bool IsConsonant(Label label){
-            if(label is null)return false;
+		public static bool IsConsonant(Label label){
+			if(label is null)return false;
 			return IsConsonant(label.Phoneme);
 		}
 
-        /// <summary>
+		/// <summary>
 		/// 鼻音かどうか
 		/// </summary>
 		/// <param name="nText"></param>
 		/// <returns></returns>
-        public static bool IsNasal(string? nText){
-            if(string.IsNullOrEmpty(nText)) return false;
+		public static bool IsNasal(string? nText){
+			if(string.IsNullOrEmpty(nText)) return false;
 			return NASAL_JA.IsMatch(nText);
 		}
 
-        /// <summary>
+		/// <summary>
 		/// ラベルの音素が鼻音かどうか
 		/// </summary>
 		/// <param name="label"></param>
 		/// <returns></returns>
-        public static bool IsNasal(Label label){
-            if(label is null)return false;
+		public static bool IsNasal(Label label){
+			if(label is null)return false;
 			return IsNasal(label.Phoneme);
 		}
 
-        /// <summary>
+		/// <summary>
 		/// 促音かどうか
 		/// </summary>
 		/// <param name="text"></param>
 		/// <returns></returns>
-        public static bool IsCL(string? text){
-            if(string.IsNullOrEmpty(text))return false;
+		public static bool IsCL(string? text){
+			if(string.IsNullOrEmpty(text))return false;
 			return text == CL;
 		}
 
-        /// <summary>
+		/// <summary>
 		/// ラベルの音素が促音かどうか
 		/// </summary>
 		/// <param name="label"></param>
 		/// <returns></returns>
-        public static bool IsCL(Label label){
-            if(label is null)return false;
+		public static bool IsCL(Label label){
+			if(label is null)return false;
 			return IsCL(label.Phoneme);
 		}
 
-        public static bool IsPau(Label label){
-            if(label is null)return false;
+		public static bool IsPau(Label label){
+			if(label is null)return false;
 			return label.Phoneme == PAU;
 		}
 	}
