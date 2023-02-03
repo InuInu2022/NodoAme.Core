84bd8088749a68b442ede7195279147321ffa238
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-04 07:26:31 +0900

    format

--- NodoAme.WPF/ViewModels/SongSoftTracFileExtSetting.cs
+++ NodoAme.WPF/ViewModels/SongSoftTracFileExtSetting.cs
@@ -1,12 +1,9 @@
-using System.Threading.Tasks;
 using Epoxy;
-using NodoAme.Models;
 
-namespace NodoAme.ViewModels
-{
-	[ViewModel]
-	public class SongSoftTracFileExtSetting{
-		public string? SongSoft { get; set; }
-		public string? FileExt { get; set; } = "ccst";
-	}
+namespace NodoAme.ViewModels;
+
+[ViewModel]
+public class SongSoftTracFileExtSetting{
+	public string? SongSoft { get; set; }
+	public string? FileExt { get; set; } = "ccst";
 }
