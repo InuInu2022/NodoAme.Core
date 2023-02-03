d394f7c032fede68262013095cd4ec3bfaf67d98
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-04 07:26:11 +0900

    format

--- NodoAme.WPF/ViewModels/SongSoftTracFileExtSetting.cs
+++ NodoAme.WPF/ViewModels/SongSoftTracFileExtSetting.cs
@@ -8,14 +8,5 @@ namespace NodoAme.ViewModels
 	public class SongSoftTracFileExtSetting{
 		public string? SongSoft { get; set; }
 		public string? FileExt { get; set; } = "ccst";
-
-
-		/*
-		[PropertyChanged(nameof(FileExt))]
-		private async ValueTask FileExtChangedAsync(string value){
-			UserSettings.FileExt = value;
-			await UserSettings.SaveAsync();
-		}
-		*/
 	}
 }
