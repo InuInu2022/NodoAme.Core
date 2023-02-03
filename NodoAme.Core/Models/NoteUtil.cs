aacd16e4f28bd1eebeb9cab43a8c1006a549724e
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-04 05:25:31 +0900

    format

--- NodoAme.Core/Models/NoteUtil.cs
+++ NodoAme.Core/Models/NoteUtil.cs
@@ -1,63 +1,66 @@
 using System;
 
-namespace NodoAme.Models
+namespace NodoAme.Models;
+
+public static class NoteUtil
 {
-    public static class NoteUtil
-    {
-        public static int FreqToMidiNoteNumber(double freq)
-		{
-			int midinote = (int)Math.Round(69.0 + (12.0 * Math.Log(freq / 440.0, 2)));
-			return midinote;
-		}
+	public static int FreqToMidiNoteNumber(double freq)
+	{
+		int midinote = (int)Math.Round(69.0 + (12.0 * Math.Log(freq / 440.0, 2)));
+		return midinote;
+	}
 
-        public static (int octave, int step) FreqToPitchOctaveAndStep(double freq){
-			int num = FreqToMidiNoteNumber(freq);
-			var oc = num == 0 ? -1 : (num / 12) - 1;
-			var st = num == 0 ? 0 : num % 12;
-			return (oc, st);
-		}
+	public static (int octave, int step) FreqToPitchOctaveAndStep(double freq)
+	{
+		int num = FreqToMidiNoteNumber(freq);
+		var oc = num == 0 ? -1 : (num / 12) - 1;
+		var st = num == 0 ? 0 : num % 12;
+		return (oc, st);
+	}
 
-		/// <summary>
-		/// Get MIDI Tick duration
-		/// </summary>
-		/// <param name="serifLen">serif duration seconds.</param>
-		/// <returns></returns>
-		public static double GetTickDuration(double serifLen)
-		{
-			return 960 / 0.4 * serifLen;
-		}
+	/// <summary>
+	/// Get MIDI Tick duration
+	/// </summary>
+	/// <param name="serifLen">serif duration seconds.</param>
+	/// <returns></returns>
+	public static double GetTickDuration(double serifLen)
+	{
+		return 960 / 0.4 * serifLen;
+	}
+}
 
-    }
+public enum NoteAdaptMode
+{
+	/// <summary>
+	/// 固定
+	/// </summary>
+	FIXED,
 
-    public enum NoteAdaptMode{
-        /// <summary>
-		/// 固定
-		/// </summary>
-        FIXED,
-        /// <summary>
-		/// 平均
-		/// </summary>
-        AVERAGE,
-        /// <summary>
-		/// 中央値
-		/// </summary>
-        MEDIAN
-    }
+	/// <summary>
+	/// 平均
+	/// </summary>
+	AVERAGE,
 
-    public enum NoteSplitModes{
-        /// <summary>
-		/// sil, pauなどを無視し、分割する
-		/// </summary>
-        IGNORE_NOSOUND,
+	/// <summary>
+	/// 中央値
+	/// </summary>
+	MEDIAN
+}
 
-        /// <summary>
-		/// sil, pauは分割に使うが無視しない
-		/// </summary>
-        SPLIT_ONLY,
+public enum NoteSplitModes
+{
+	/// <summary>
+	/// sil, pauなどを無視し、分割する
+	/// </summary>
+	IGNORE_NOSOUND,
 
-		/// <summary>
-        /// 音節毎で分割、sil, pauなどを無視
-        /// </summary>
-		SYLLABLE_IGNORE_NOSOUND,
-    }
+	/// <summary>
+	/// sil, pauは分割に使うが無視しない
+	/// </summary>
+	SPLIT_ONLY,
+
+	/// <summary>
+	/// 音節毎で分割、sil, pauなどを無視
+	/// </summary>
+	SYLLABLE_IGNORE_NOSOUND,
 }
