adc2982f83eae89855d0f9b065720b6889bbac1b
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-06 14:50:01 +0900

    add SoundFile type for wav file

--- NodoAme.Core/Models/TalkSoft/TalkEngine.cs
+++ NodoAme.Core/Models/TalkSoft/TalkEngine.cs
@@ -7,4 +7,9 @@ public static class TalkEngine{
 	public const string CEVIO = "CeVIO";
 	public const string OPENJTALK = "OpenJTalk";
 	public const string VOICEVOX = "VOICEVOX";
+
+	/// <summary>
+    /// export from file (wav& lab)
+    /// </summary>
+	public const string SOUNDFILE = "SoundFile";
 }
