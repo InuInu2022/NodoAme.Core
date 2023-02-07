f119afa6e3f495432c79038cd60da1e47e2caf63
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-07 10:05:50 +0900

    Add drop event and fix minor

--- NodoAme.WPF/ViewModels/SerifViewModel.cs
+++ NodoAme.WPF/ViewModels/SerifViewModel.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,22 +13,25 @@ public class SerifViewModel
 {
 	public string? SourceText { get; set; }
 	public string? ConvertedText { get; set; }
-
 	public string? SerifTime { get; set; }
+	public string DropFileName { get; set; } = "";
 	public MainWindowViewModel? ParentVM { get; set; }
-
 	public bool EnabledSerifInput { get; set; }
 	public bool EnabledPreview { get; set; }
 	public bool EnabledExport { get; set; }
-
-
 	public Visibility ExportProgress { get; set; } = Visibility.Collapsed;
-
 	public Visibility PreviewProgress { get; set; } = Visibility.Collapsed;
-
 	public Command PreviewTalk { get; set; }
 	public Command ExportTrackFile { get; set; }
+	public Command Drop { get; set; }
 
+	public Visibility SourceTextVisible { get; set; }
+		= Visibility.Visible;
+
+	public Visibility DropFileNameView { get; set; }
+		= Visibility.Collapsed;
+
+	public Command ResetDropFile { get; set; }
 	public Command ExportPreviewWav { get; set; }
 	public Command CheckEnterAndAddRow { get; private set; }
 	private string? LastSourceText { get; set; }
@@ -37,6 +41,11 @@ public class SerifViewModel
 	{
 		this.PreviewTalk = CommandFactory.Create<RoutedEventArgs>(
 			async _ => {
+				if(SourceText is null)
+				{
+					return;
+				}
+
 				EnabledPreview = false;
 				EnabledExport = false;
 				PreviewProgress = Visibility.Visible;
@@ -49,14 +58,19 @@ public class SerifViewModel
 
 		this.ExportTrackFile = CommandFactory.Create<RoutedEventArgs>(async _ =>
 		{
+			if(SourceText is null)
+			{
+				return;
+			}
+
 			EnabledExport = false;
 			EnabledPreview = false;
 			ExportProgress = Visibility.Visible;
-			var cast = ParentVM!.ExportSongCastItems[ParentVM.ExportSongCastSelected];
+			var cast = ParentVM!.ExportSongCastItems![ParentVM.ExportSongCastSelected];
 			const double alpha = 0.0;	//TODO:
 			await ParentVM!.ExportFileFromListAsync(
 				this.SourceText,
-				cast.Id,
+				cast.Id!,
 				alpha,
 				isTrack: true,
 				songCast: cast
@@ -66,11 +80,55 @@ public class SerifViewModel
 			ExportProgress = Visibility.Collapsed;
 		});
 
+		Drop = CommandFactory
+			.Create<DragEventArgs>(
+				async e =>
+				{
+					if (!e.Data.GetDataPresent(DataFormats.FileDrop)){
+						return;
+					}
+
+					string[] paths = ((string[])e.Data.GetData(DataFormats.FileDrop));
+					await Task.Run(() =>
+						Debug.WriteLine($"dropfile:{paths[0]}"));
+
+					var target = paths[0];
+					DropFileName = Path.GetFileName(target);
+					var lab = Path.ChangeExtension(target, "lab");
+					var labExists = File.Exists(lab);
+					if(labExists){
+						SourceTextVisible = Visibility.Collapsed;
+						DropFileNameView = Visibility.Visible;
+						//TODO:
+
+					}else{
+						SourceTextVisible = Visibility.Collapsed;
+						DropFileNameView = Visibility.Visible;
+						EnabledExport = false;
+						DropFileName = $"⚠️{Path.GetFileName(target)}";
+						ConvertedText = $"lab file {lab} is NOT FOUND!";
+					}
+				});
+
+		ResetDropFile =
+			Command.Factory.CreateSync(
+				() =>
+				{
+					DropFileName = "";
+					SourceTextVisible = Visibility.Visible;
+					DropFileNameView = Visibility.Collapsed;
+					EnabledExport = true;
+					ConvertedText = "";
+				}
+			);
+
 		ExportPreviewWav =
 			CommandFactory
 				.Create<RoutedEventArgs>(
 					async _ =>
 					{
+						if(SourceText is null)return;
+
 						EnabledExport = false;
 						//TODO:enable loading indicater
 						await ParentVM!
@@ -109,7 +167,7 @@ public class SerifViewModel
 
 		//同じ文字列なら処理を抜ける
 		if(this.LastSourceText == sourceText
-			&& this.LastCast != ParentVM!.ExportSongCastItems[ParentVM.ExportSongCastSelected]) {return;}
+			&& this.LastCast != ParentVM!.ExportSongCastItems![ParentVM.ExportSongCastSelected]) {return;}
 
 		this.LastSourceText = sourceText;
 		this.ConvertedText = await ParentVM!.ConvertFromListAsync(sourceText);
