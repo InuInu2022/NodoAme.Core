d08007ba0110a07ca205b5739d1b604a30c9964c
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2026-03-22 11:46:09 +0900

    fix: clean up formatting and improve documentation in Label class

--- NodoAme.Core/Models/Label.cs
+++ NodoAme.Core/Models/Label.cs
@@ -1,27 +1,21 @@
-using System;
+namespace NodoAme.Models;
 
-namespace NodoAme.Models
+/// <summary>
+/// ラベルデータ管理クラス
+/// </summary>
+public class Label
 {
-    /// <summary>
-	/// ラベルデータ管理クラス
+	/// <summary>
+	/// 音素
 	/// </summary>
-    public class Label
-    {
-        /// <summary>
-		/// 音素
-		/// </summary>
-        public string? Phoneme { get; set; } = "xx";
-		public double? StartTime { get; set; }
-		public double? EndTime { get; set; }
+	public string? Phoneme { get; set; } = "xx";
+	public double? StartTime { get; set; }
+	public double? EndTime { get; set; }
 
-        public Label(
-            string phoneme,
-            double startTime,
-            double endTime
-        ){
-			Phoneme = phoneme;
-			StartTime = startTime;
-			EndTime = endTime;
-		}
-    }
+	public Label(string phoneme, double startTime, double endTime)
+	{
+		Phoneme = phoneme;
+		StartTime = startTime;
+		EndTime = endTime;
+	}
 }
