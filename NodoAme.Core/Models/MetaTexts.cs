4176f1ad1e4ecd7a68c8070f70a7dc10e4cfc427
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2026-03-22 11:46:18 +0900

    fix: adjust namespace declaration and simplify MetaTexts class structure

--- NodoAme.Core/Models/MetaTexts.cs
+++ NodoAme.Core/Models/MetaTexts.cs
@@ -1,14 +1,11 @@
-#nullable enable
+namespace NodoAme.Models;
 
-namespace NodoAme
+public static class MetaTexts
 {
-	public static class MetaTexts
-	{
-		public const string SERIF = "$セリフ$";
-		public const string CASTNAME = "$キャスト名$";
-		public const string DATE = "$日付$";
-		public const string NUMBER = "$連番$";
-		public const string TRACKNAME = "$トラック名$";
+	public const string SERIF = "$セリフ$";
+	public const string CASTNAME = "$キャスト名$";
+	public const string DATE = "$日付$";
+	public const string NUMBER = "$連番$";
+	public const string TRACKNAME = "$トラック名$";
 
-	}
 }
